d50027c7a727a4cec49ba9669bf6c179fa5dc920
Taskoudis Dimitris <63710755+Taskoudis-Dimi@users.noreply.github.com>
2020-09-07 17:46:38 +0300

    Add files via upload

---
+++ Machine_translation_Fragkouli_Taskoudis.py
@@ -0,0 +1,397 @@
+﻿
+#download dataset
+#unzip dataset
+!unzip "/content/drive/My Drive/Κοτρο/deu-eng.zip" -d "content"
+
+#import libraries
+import string
+import re
+from pickle import dump
+from unicodedata import normalize
+from numpy import array
+ 
+#load the data in a way that preserves the Unicode German characters
+def load_doc(filename):
+	# open the file as read text (rt) only
+	file = open(filename, mode='rt', encoding='utf-8')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+ 
+# split a loaded document into sentences
+def to_pairs(doc):
+	lines = doc.strip().split('\n')
+	pairs = [line.split('\t') for line in  lines]
+	return pairs
+ 
+# clean a list of lines
+def clean_pairs(lines):
+	cleaned = list()
+	# prepare regex for char filtering
+	re_print = re.compile('[^%s]' % re.escape(string.printable)) #remove all non-alphanumerics backslashed
+	# prepare translation table for removing punctuation
+	table = str.maketrans('', '', string.punctuation) #Return a translation table usable for str.translate()
+	for pair in lines:
+		clean_pair = list()
+		for line in pair:
+			# normalize unicode characters
+			line = normalize('NFD', line).encode('ascii', 'ignore') #convert all those escape characters into their respective characters like if there is an unicode à, how do i convert that into a standard
+			line = line.decode('UTF-8') #decode the source text. Most likely the source text is in UTF-8 format, not ASCII
+			# tokenize on white space
+			line = line.split()
+			# convert to lowercase
+			line = [word.lower() for word in line] #converts all uppercase characters in a string into lowercase characters
+			# remove punctuation from each token
+			line = [word.translate(table) for word in line] #returns a string where each character is mapped to its corresponding character as per the translation table
+			# remove non-printable chars form each token
+			line = [re_print.sub('', w) for w in line]
+			# remove tokens with numbers in them
+			line = [word for word in line if word.isalpha()] #checks whether the string consists of alphabetic characters only
+			# store as string
+			clean_pair.append(' '.join(line))
+		cleaned.append(clean_pair)
+	return array(cleaned)
+ 
+# save a list of clean sentences to file
+def save_clean_data(sentences, filename):
+	dump(sentences, open(filename, 'wb')) #The dump() method is used when the Python objects have to be stored in a file
+	print('Saved: %s' % filename)
+ 
+# load dataset
+filename = 'deu.txt'
+doc = load_doc(filename)
+# split into english-german pairs
+pairs = to_pairs(doc)
+# clean sentences
+clean_pairs = clean_pairs(pairs)
+# save clean pairs to file
+save_clean_data(clean_pairs, 'english-german.pkl')
+# spot check
+for i in range(100):
+	print('[%s] => [%s]' % (clean_pairs[i,0], clean_pairs[i,1]))
+
+#import libraries
+from pickle import load
+from pickle import dump
+from numpy.random import rand
+from numpy.random import shuffle
+ 
+# load a clean dataset
+def load_clean_sentences(filename):
+	return load(open(filename, 'rb')) #read binary sentences
+ 
+# save a list of clean sentences to file
+def save_clean_data(sentences, filename):
+	dump(sentences, open(filename, 'wb')) #The dump() method is used when the Python objects have to be stored in a file
+	print('Saved: %s' % filename)
+ 
+# load dataset
+raw_dataset = load_clean_sentences('english-german.pkl')
+ 
+# reduce dataset size
+n_sentences = 10000
+dataset = raw_dataset[:n_sentences, :]
+
+# random shuffle
+shuffle(dataset)
+
+# split into train(9000)/test(1000)
+train, test = dataset[:9000], dataset[9000:]
+
+# save sum, train and test dataset
+save_clean_data(dataset, 'english-german-both.pkl')
+save_clean_data(train, 'english-german-train.pkl')
+save_clean_data(test, 'english-german-test.pkl')
+
+# load a clean dataset
+def load_clean_sentences(filename):
+	return load(open(filename, 'rb'))
+
+# load datasets from content
+dataset = load_clean_sentences('english-german-both.pkl')
+train = load_clean_sentences('english-german-train.pkl')
+test = load_clean_sentences('english-german-test.pkl')
+
+# create a tokenizer and finding frequency of each word
+def create_tokenizer(lines):
+	tokenizer = Tokenizer()
+	tokenizer.fit_on_texts(lines) #This method creates the vocabulary index based on word frequency. Lower integer means more frequent word
+	return tokenizer
+
+# max sentence length
+def max_length(lines):
+	return max(len(line.split()) for line in lines)
+
+#import library
+from keras.preprocessing.text import Tokenizer
+
+# prepare english tokenizer
+eng_tokenizer = create_tokenizer(dataset[:, 0]) #This class allows to vectorize a text corpus, by turning each text into either a sequence of integers (each integer being the index of a token in a dictionary)
+#or into a vector where the coefficient for each token could be binary, based on word count.
+
+#find voc size and size of max length
+eng_vocab_size = len(eng_tokenizer.word_index) + 1
+eng_length = max_length(dataset[:, 0])
+
+print('English Vocabulary Size: %d' % eng_vocab_size)
+print('English Max Length: %d' % (eng_length))
+
+# prepare german tokenizer
+ger_tokenizer = create_tokenizer(dataset[:, 1])
+
+#find voc size and size of max length
+ger_vocab_size = len(ger_tokenizer.word_index) + 1
+ger_length = max_length(dataset[:, 1])
+
+print('German Vocabulary Size: %d' % ger_vocab_size)
+print('German Max Length: %d' % (ger_length))
+
+# encode and pad sequences
+def encode_sequences(tokenizer, length, lines):
+	# integer encode sequences
+	X = tokenizer.texts_to_sequences(lines)
+	# converts every sequence of the X list to a array with a length size, filling in the last positions of each table with zeros
+	X = pad_sequences(X, maxlen=length, padding='post') 
+	return X
+
+#The output sequence needs to be one-hot encoded. This is because 
+#the model will predict the probability of each word in the vocabulary as output.
+#one hot encode target sequence
+
+#Require all input variables and output variables to be numeric. 
+#The integer encoded variable is removed and a new binary variable is added for each unique integer value.
+
+def encode_output(sequences, vocab_size):
+	ylist = list()
+	for sequence in sequences:
+		encoded = to_categorical(sequence, num_classes=vocab_size) #We use the to_categorical() function in the Keras API to one hot encode the output sequences.
+		ylist.append(encoded)
+	y = array(ylist)
+	y = y.reshape(sequences.shape[0], sequences.shape[1], vocab_size)
+	return y
+
+#import libraries
+from keras.utils.np_utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
+
+
+# prepare training data
+trainX = encode_sequences(ger_tokenizer, ger_length, train[:, 1])
+trainY = encode_sequences(eng_tokenizer, eng_length, train[:, 0])
+trainY = encode_output(trainY, eng_vocab_size) 
+# prepare validation data
+testX = encode_sequences(ger_tokenizer, ger_length, test[:, 1])
+testY = encode_sequences(eng_tokenizer, eng_length, test[:, 0])
+testY = encode_output(testY, eng_vocab_size)
+
+#import libraries
+from keras.layers import Embedding
+from keras.layers import Dense, LSTM, TimeDistributed
+from keras.layers import RepeatVector
+from keras.utils import plot_model
+from keras.models import Sequential
+
+
+# define NMT model
+def define_model(src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units):
+	model = Sequential()
+	model.add(Embedding(src_vocab, n_units, input_length=src_timesteps, mask_zero=True)) #Turns positive integers (indexes) into dense vectors of fixed size
+	model.add(LSTM(n_units)) #Long Short-Term Memory layer 
+	model.add(RepeatVector(tar_timesteps)) #Repeats the input "tar_timesteps" times
+	model.add(LSTM(n_units, return_sequences=True))
+	model.add(TimeDistributed(Dense(tar_vocab, activation='softmax'))) #This wrapper allows to apply a layer to every temporal slice of an input
+	return model
+ 
+# define model
+model = define_model(ger_vocab_size, eng_vocab_size, ger_length, eng_length, 256)
+
+#define optimizer and loss function
+model.compile(optimizer='adam', loss='categorical_crossentropy')
+
+# summarize defined model
+print(model.summary())
+#plot image of model and sum of parameters
+plot_model(model, to_file='model.png', show_shapes=True) #plot image of model architecture
+
+#improt library
+from keras.callbacks import ModelCheckpoint
+
+filename = 'model.h5'
+
+#Checkpoint is setup to save the network weights only when there is an improvement in loss on the validation dataset
+checkpoint = ModelCheckpoint("/content/english-german-train.pkl", monitor='val_loss', verbose=1, save_best_only=True, mode='min') 
+#fit model
+model.fit(trainX, trainY, epochs=30, batch_size=64, validation_data=(testX, testY), callbacks=[checkpoint], verbose=2)
+
+
+#save weights and model after training
+model.save_weights("model.h5")
+model.save("model.h5")
+print("Saved model")
+
+#import libraries
+from pickle import load
+from numpy import array
+from numpy import argmax
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+from nltk.translate.bleu_score import corpus_bleu
+
+# load datasets
+dataset = load_clean_sentences('english-german-both.pkl')
+#train = load_clean_sentences('english-german-train.pkl')
+test = load_clean_sentences('english-german-test.pkl')
+# prepare english tokenizer
+eng_tokenizer = create_tokenizer(dataset[:, 0])
+eng_vocab_size = len(eng_tokenizer.word_index) + 1
+eng_length = max_length(dataset[:, 0])
+# prepare german tokenizer
+ger_tokenizer = create_tokenizer(dataset[:, 1])
+ger_vocab_size = len(ger_tokenizer.word_index) + 1
+ger_length = max_length(dataset[:, 1])
+# prepare data
+#trainX = encode_sequences(ger_tokenizer, ger_length, train[:, 1])
+testX = encode_sequences(ger_tokenizer, ger_length, test[:, 1])
+
+from keras.models import load_model
+# load model
+model = load_model('model.h5')
+
+#model predictions for test data
+translation = model.predict(testX, verbose=0)
+
+#map an integer to a word
+def word_for_id(integer, tokenizer):
+	for word, index in tokenizer.word_index.items():
+		if index == integer:
+			return word
+	return None
+
+# generate target given source sequence
+def predict_sequence(model, tokenizer, source):
+	prediction = model.predict(source, verbose=0)[0]
+	integers = [argmax(vector) for vector in prediction]
+	target = list()
+	for i in integers:
+		word = word_for_id(i, tokenizer)
+		if word is None:
+			break
+		target.append(word)
+	return ' '.join(target)
+
+#evaluate the skill of the model
+def evaluate_model(model, tokenizer, sources, raw_dataset):
+	actual, predicted = list(), list()
+	for i, source in enumerate(sources):
+		# translate encoded source text
+		source = source.reshape((1, source.shape[0]))
+		translation = predict_sequence(model, eng_tokenizer, source)
+		raw_target, raw_src = raw_dataset[i]
+		if i < 10:
+			print('src=[%s], target=[%s], predicted=[%s]' % (raw_src, raw_target, translation))
+		actual.append([raw_target.split()])
+		predicted.append(translation.split())
+	# calculate BLEU score
+	print('BLEU-1: %f' % corpus_bleu(actual, predicted, weights=(1.0, 0, 0, 0)))
+	print('BLEU-2: %f' % corpus_bleu(actual, predicted, weights=(0.5, 0.5, 0, 0)))
+	print('BLEU-3: %f' % corpus_bleu(actual, predicted, weights=(0.3, 0.3, 0.3, 0)))
+	print('BLEU-4: %f' % corpus_bleu(actual, predicted, weights=(0.25, 0.25, 0.25, 0.25)))
+
+print ("test shape is:", test.shape)
+
+#import libraries
+from pickle import load
+from numpy import array
+from numpy import argmax
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+from nltk.translate.bleu_score import corpus_bleu
+ 
+# load a clean dataset
+def load_clean_sentences(filename):
+	return load(open(filename, 'rb'))
+ 
+# fit a tokenizer
+def create_tokenizer(lines):
+	tokenizer = Tokenizer()
+	tokenizer.fit_on_texts(lines)
+	return tokenizer
+ 
+# max sentence length
+def max_length(lines):
+	return max(len(line.split()) for line in lines)
+ 
+# encode and pad sequences
+def encode_sequences(tokenizer, length, lines):
+	# integer encode sequences
+	X = tokenizer.texts_to_sequences(lines)
+	# pad sequences with 0 values
+	X = pad_sequences(X, maxlen=length, padding='post')
+	return X
+ 
+# map an integer to a word
+def word_for_id(integer, tokenizer):
+	for word, index in tokenizer.word_index.items():
+		if index == integer:
+			return word
+	return None
+ 
+# generate target given source sequence
+def predict_sequence(model, tokenizer, source):
+	prediction = model.predict(source, verbose=0)[0]
+	integers = [argmax(vector) for vector in prediction]
+	target = list()
+	for i in integers:
+		word = word_for_id(i, tokenizer)
+		if word is None:
+			break
+		target.append(word)
+	return ' '.join(target)
+ 
+# evaluate the skill of the model
+def evaluate_model(model, tokenizer, sources, raw_dataset):
+	actual, predicted = list(), list()
+	for i, source in enumerate(sources):
+		# translate encoded source text
+		source = source.reshape((1, source.shape[0]))
+		translation = predict_sequence(model, eng_tokenizer, source)
+		raw_target, raw_src, test = raw_dataset[i]
+		if i < 10:
+			print('src=[%s], target=[%s], predicted=[%s]' % (raw_src, raw_target, translation))
+		actual.append([raw_target.split()])
+		predicted.append(translation.split())
+	# calculate BLEU score
+	print('BLEU-1: %f' % corpus_bleu(actual, predicted, weights=(1.0, 0, 0, 0)))
+	print('BLEU-2: %f' % corpus_bleu(actual, predicted, weights=(0.5, 0.5, 0, 0)))
+	print('BLEU-3: %f' % corpus_bleu(actual, predicted, weights=(0.3, 0.3, 0.3, 0)))
+	print('BLEU-4: %f' % corpus_bleu(actual, predicted, weights=(0.25, 0.25, 0.25, 0.25)))
+ 
+# load datasets
+dataset = load_clean_sentences('english-german-both.pkl')
+train = load_clean_sentences('english-german-train.pkl')
+test = load_clean_sentences('english-german-test.pkl')
+# prepare english tokenizer
+eng_tokenizer = create_tokenizer(dataset[:, 0])
+eng_vocab_size = len(eng_tokenizer.word_index) + 1
+eng_length = max_length(dataset[:, 0])
+# prepare german tokenizer
+ger_tokenizer = create_tokenizer(dataset[:, 1])
+ger_vocab_size = len(ger_tokenizer.word_index) + 1
+ger_length = max_length(dataset[:, 1])
+# prepare data
+trainX = encode_sequences(ger_tokenizer, ger_length, train[:, 1])
+testX = encode_sequences(ger_tokenizer, ger_length, test[:, 1])
+ 
+# load model
+model = load_model('model.h5')
+# test on some training sequences
+print('train')
+evaluate_model(model, eng_tokenizer, trainX, train)
+# test on some test sequences
+print('test')
+evaluate_model(model, eng_tokenizer, testX, test)
+
---
+++ Readme.txt
@@ -0,0 +1,24 @@
+REFERENCES
+[1] Brownlee,Jason, “A Gentle Introduction to Neural Machine Translation”,
+https://machinelearningmastery.com/introduction-neural-machinetranslation//
+[2] Brown, Peter F. and Cocke, John and Della Pietra, Stephen A. and Della
+Pietra, Vincent J. and Jelinek, Fredrick and Lafferty, John D. and Mercer,
+Robert L. and Roossin, Paul S., ”A Statistical Approach to Machine
+Translation”, Computational Linguistics,1990
+[3] Brownlee,Jason, “What Are Word Embeddings for Text?”,
+https://machinelearningmastery.com/what-are-word-embeddings/
+[4] Dzmitry Bahdanau and Kyunghyun Cho and Yoshua Bengio, ”Neural
+Machine Translation by Jointly Learning to Align and Translate”, 2014
+[5] Brownlee,Jason, “How to Develop a Neural Machine Translation System
+from Scratch”, https://machinelearningmastery.com/develop-neuralmachine-
+translation-system-keras.
+[6] Brownlee,Jason, “How to Configure an Encoder-
+Decoder Model for Neural Machine Translation”,
+https://machinelearningmastery.com/configure-encoder-decoder-modelneural-
+machine-translation/
+[7] Brownlee,Jason, “A Gentle Introduction to Calculating the BLEU Score
+for Text in Python”, https://machinelearningmastery.com/calculate-bleuscore-
+for-text-python/
+[8] Tatman, Rchael, “Evaluating Text Output in NLP: BLEU at your own
+risk”, https://towardsdatascience.com/evaluating-text-output-in-nlp-bleuat-
+your-own-risk-e8609665a213
